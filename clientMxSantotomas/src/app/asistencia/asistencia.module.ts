2b6ffce8660f1e8af3ef012f16a96b3c598980f3
Rudy Apaza <rudyapazay@gmail.com>
2019-09-06 15:45:43 -0500

    asistencia

---
+++ clientMxSantotomas/src/app/asistencia/asistencia.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { AsistenciaRoutingModule } from './asistencia-routing.module';
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    AsistenciaRoutingModule
+  ]
+})
+export class AsistenciaModule { }
