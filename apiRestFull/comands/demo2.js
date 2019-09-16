59de37fcc28c0e339b5908d3b49b8a3ad3167240
Rudy Apaza <rudyapazay@gmail.com>
2019-09-16 16:07:25 -0500

    familiaEstudiante

--- apiRestFull/comands/demo2.js
+++ apiRestFull/comands/demo2.js
@@ -1,2 +1,24 @@
-var fecha = new Date();
-console.log(fecha.getHours());
+'use strict'
+
+var mongoose = require('mongoose');
+
+var EstudianteMDL = require('../models/estudiante.model');
+var FamiliaMDL = require('../models/familia.model');
+
+mongoose.connect('mongodb://localhost:27017/appAdmEstMxST',{useNewUrlParser: true}, (err,res)=>{
+    if(err){
+        throw err;
+    }
+    else{
+        /**/
+        FamiliaMDL.find().populate({path:'estudiante'}).limit(1).exec((err, familia)=>{
+          console.log(result);
+          console.log(familia);
+        });
+        /**/
+       /*EstudianteMDL.find().populate({path:'familia'}).limit(1).exec((err, estudiante)=>{
+         console.log(estudiante);
+       })
+       */
+    }
+});
