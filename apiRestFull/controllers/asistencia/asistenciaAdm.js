ee61b1dc2f6d9f92c41520b5fd99411399e3315b
Rudy Apaza <rudyapazay@gmail.com>
2019-10-04 19:13:26 -0500

    Asistencia XO

---
+++ apiRestFull/controllers/asistencia/asistenciaAdm.js
@@ -0,0 +1,18 @@
+
+// administracion de asistencia
+async function iniciarDia (){
+  //var asistencia  = await AsistenciaMdl.find().limit(10);
+  /*
+  var estudiantes =  await EstudianteMdl.find();
+  var i = 0;
+  while(estudiantes[i]){
+    var  asistencia  = new AsistenciaMdl();
+    asistencia.estudiante = estudiantes[i]._id;
+    await asistencia.save();
+    i++;
+  }
+  var ast = await AsistenciaMdl.find().limit(10);
+  var id = req.params.id;
+  */
+
+}
